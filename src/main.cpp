445e343900415a1970169dd62742e0206b7764b4
Fallingnight <fallingnight131@outlook.com>
2025-03-15 01:52:14 +0800

    修改cmakelist配置，使其避免乱码问题

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2\imgproc\types_c.h>
+#include <mutex>
 #include<iostream>
 #include<ctime>
 #include "../include/ImageInformation.h"
